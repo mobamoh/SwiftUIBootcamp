38161196b4a4d5641c3ce2e8ec456ac258322663
Mohamed.Bamoh <mbamoh@icloud.com>
2025-08-14 13:20:06 +0200

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by mohamedbamoh on 14.08.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
